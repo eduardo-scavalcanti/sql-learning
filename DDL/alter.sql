9fbc49ddf93da120d89aeb39f821cb74463b2550
Eduardo de Souza Cavalcanti Junior <eduardoscavalcantij@gmail.com>
2025-08-22 11:53:18 -0300

    improving text

--- DDL/alter.sql
+++ DDL/alter.sql
@@ -1,10 +1,14 @@
 /*
-The "ALTER" command is used to modify database objects in SQL. Let's add a column to the table "customers" that we created in the "create.sql" file.
+The "ALTER" command is used to modify database objects in SQL.
+
+There isn't a specific syntax for the 'ALTER' command, since it can be used to modify different types of database objects. Therefore, each object has its own syntax.
+
+Based on the 'create.sql' file, let’s focus on a table.
 Syntax:
 ALTER TABLE table_name
 ADD column_name data_type constraints
 
-It's pretty similar to the "CREATE" command, right? Let's see an example.
+In the example below, we’ll add a column to the "customers" table we created before.
 Example:
 */
 ALTER TABLE customers
@@ -18,7 +22,7 @@ Syntax:
 ALTER TABLE table_name
 DROP COLUMN column_name
 
-As you can see, unlike adding a column, when deleting we need to explicitly tell the DROP clause that we want to remove a column.
+As you can see, unlike adding a column, when deleting we need to explicitly tell the "DROP" clause that we want to remove a column.
 
 Example:
 */
@@ -27,4 +31,6 @@ DROP COLUMN birth_date
 
 /*
 Observation: be careful all the data in the column will be lost.
+
+So that’s it! Feel free to look up how to modify other database objects.
 */
