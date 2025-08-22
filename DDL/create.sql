a9711cff121156a4e4d68add5753620f52abf26f
Eduardo de Souza Cavalcanti Junior <eduardoscavalcantij@gmail.com>
2025-08-22 11:41:59 -0300

    improving text

--- DDL/create.sql
+++ DDL/create.sql
@@ -1,25 +1,29 @@
 /*
-The "CREATE" command is used to create database objects in SQL. For now, let’s focus on creating tables.
+The "CREATE" command is used to create database objects in SQL.
+
+For now, let’s focus on creating tables.
 Syntax:
 CREATE TABLE table_name(
     first_column_name data_type constraints,
     second_column_name data_type constraints,
     Nth_column_name data_type constraints
-    )
+    );
 
-Maybe you’re thinking it looks complex, right? But let’s see how it’s done.
+That said, let's look at an example.
 Example:
 */
 CREATE TABLE customers(
     customer_id INT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NULL
-    )
+    );
 /*
 In the example above, we are creating a table called "customers" and adding three (3) columns to it.
-The first column is called "customer_id"; its data type is "INT" (integer), which means an integer number value, and it has a constraint called "PRIMARY KEY" that I’ll explain further.
-The second column is called "first_name"; its data type is "VARCHAR(50)", which means a text or string value with a length of up to 50 characters, and it has a constraint called "NOT NULL", meaning the value cannot be empty.
-The third and last column is called "last_name"; its data type is "VARCHAR(50)", the same length as the second column, and it has a constraint called "NULL", which means this value can be empty.
+The first column is called "customer_id", its data type is "INT" (integer), which means an integer number value, and it has a constraint called "PRIMARY KEY" that I’ll explain further.
+
+The second column is called "first_name", its data type is "VARCHAR(50)", which means a text or string value with a length of up to 50 characters, and it has a constraint called "NOT NULL", meaning the value cannot be empty.
+
+The third and last column is called "last_name"; its data type is "VARCHAR(50)" and it has a constraint called "NULL", which means this value can be empty.
 
 Constraints can be used to apply specific rules to a column. For example:
 "PRIMARY KEY" -> values must be unique and cannot be empty
@@ -29,5 +33,5 @@ Constraints can be used to apply specific rules to a column. For example:
 
 Observation: A column can also have more than one constraint.
 
-There are many constraints available, but the main goal here is to understand how the "CREATE" command works and when to use specific constraints.
+There are many constraints available. The main goal here is to understand how the "CREATE" command works, but feel free to look up the other constraints and try applying them to a column.
 */
